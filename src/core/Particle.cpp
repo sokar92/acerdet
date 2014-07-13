e189a0337f436423c5e3e914dab67c2fd066a3d2
Patryk Mikos <patryk912@gmail.com>
2014-07-13 13:35:56 +0200

    Particle update

---
+++ src/core/Particle.cpp
@@ -0,0 +1,64 @@
+#include "Particle.h"
+using namespace AcerDet::core;
+
+#define ParticleNull (-1)
+
+Particle::Particle() : 
+	state(PS_NULL), 
+	type(PT_UNKNOWN), 
+	px(0), py(0), pz(0), e(0), 
+	phi(0), theta(0),
+	prod_x(0), prod_y(0), prod_z(0), prod_time(0), 
+	id(-1), mother(-1), 
+	daughters(make_pair(-1,-1)) 
+{}
+
+bool Particle::hasMother() const { 
+	return mother != ParticleNull; 
+}
+
+bool Particle::hasDaughter() const { 
+	return daughters.first != ParticleNull; 
+}
+
+Int32_t Particle::daughtersCount() const {
+	if (hasDaughter())
+		return 1 + daughters.second - daughters.first;
+	return 0;
+}
+
+void Particle::print() const {
+	printf ("Particle:\n");
+	printf ("\tID: %d\n", id);
+	printf ("\tType: %s\n", getTypeName().c_str());
+	printf ("\tState: %s\n", getStateName().c_str());
+	printf ("\tPolarization (phi, theta): (%f, %f)\n", phi, theta);
+	printf ("\tMomentum (px,py,pz,e) = (%f, %f, %f, %f)\n", px, py, pz, e);
+	printf ("\tProduction (x,y,z,t) = (%f, %f, %f, %f)\n", prod_x, prod_y, prod_z, prod_time);
+	printf ("\n");
+}
+
+string Particle::getTypeName() const {
+	switch(type) {
+	case PT_JET: return string("Jet");
+	case PT_BJET: return string("B-Jet");
+	case PT_CJET: return string("C-Jet");
+	case PT_CELL: return string("Cell");
+	case PT_CLUSTER: return string("Cluster");
+	case PT_MUON: return string("Muon");
+	case PT_ELECTRON: return string("Electron");
+	case PT_PHOTON: return string("Photon");
+	case PT_TAU: return string("Tau");
+	default: return string("Unknown");
+	}
+}
+
+string Particle::getStateName() const {
+	switch(state) {
+	case PS_BEAM: return string("Beam");
+	case PS_FINAL: return string("Final");
+	case PS_DECAYED: return string("Decayed");
+	case PS_HISTORY: return string("Historical");
+	default: return string("Unknown");
+	}
+}
--- src/core/Particle.h
+++ src/core/Particle.h
@@ -42,6 +42,9 @@ namespace AcerDet {
 			PT_TAU
 		};
 		
+		/*
+		 * Common class representing partons
+		 */
 		class Particle {
 		public:
 			ParticleState state;
@@ -53,45 +56,36 @@ namespace AcerDet {
 			Int32_t id,mother;
 			pair<Int32_t,Int32_t> daughters;
 			
-			Particle() : state(PS_NULL), type(PT_UNKNOWN), px(0), py(0), pz(0), e(0), phi(0), theta(0),
-				prod_x(0), prod_y(0), prod_z(0), prod_time(0), id(-1), mother(-1), daughters(make_pair(-1,-1)) {}
-
-			void print() const {
-				printf ("Particle:\n");
-				printf ("\tID: %d\n", id);
-				printf ("\tType: %s\n", getTypeName().c_str());
-				printf ("\tState: %s\n", getStateName().c_str());
-				printf ("\tPolarization (phi, theta): (%f, %f)\n", phi, theta);
-				printf ("\tMomentum (px,py,pz,e) = (%f, %f, %f, %f)\n", px, py, pz, e);
-				printf ("\tProduction (x,y,z,t) = (%f, %f, %f, %f)\n", prod_x, prod_y, prod_z, prod_time);
-				printf ("\n");
-			}
+			/*
+			 * Default ctor
+			 * Initialize particle data
+			 */
+			Particle();
+			
+			/*
+			 * Check if given particle has mother
+			 */
+			bool hasMother() const;
+			
+			/*
+			 * Check if given particle has any daughters
+			 */
+			bool hasDaughter() const;
+			
+			/*
+			 * Number of particle daughters
+			 */
+			Int32_t daughtersCount() const;
+			
+			/*
+			 * Print basic informations about particle
+			 */
+			void print() const;
 			
 		private:
-			string getTypeName() const {
-				switch(type) {
-				case PT_JET: return string("Jet");
-				case PT_BJET: return string("B-Jet");
-				case PT_CJET: return string("C-Jet");
-				case PT_CELL: return string("Cell");
-				case PT_CLUSTER: return string("Cluster");
-				case PT_MUON: return string("Muon");
-				case PT_ELECTRON: return string("Electron");
-				case PT_PHOTON: return string("Photon");
-				case PT_TAU: return string("Tau");
-				default: return string("Unknown");
-				}
-			}
+			string getTypeName() const;
 			
-			string getStateName() const {
-				switch(state) {
-				case PS_BEAM: return string("Beam");
-				case PS_FINAL: return string("Final");
-				case PS_DECAYED: return string("Decayed");
-				case PS_HISTORY: return string("Historical");
-				default: return string("Unknown");
-				}
-			}
+			string getStateName() const;
 		};
 
 		/*
@@ -99,19 +93,7 @@ namespace AcerDet {
 		  and angle calculation.
 		*/
 
-/*		template<typename T>
-		class ParticleT {
-		private:
-			T 	_px,_py,_pz,_e;
-			T	_phi, _theta;
-			
-			Int32_t _id, _idMother;
-			pair<Int32_t,Int32_t> _idDaughter;
-			ParticleState	_state;
-		public:
-			ParticleT () : _px(0), _py(0), _pz(0), _e(0), _id(0), _state(PS_NULL) {}
-			ParticleT (T px, T py, T pz, T e, Int32_t id) : _px(px), _py(py), _pz(pz), _e(e), _id(id) {}
-
+/*
 		public:
 			T getPx() const { return _px; }
 			T getPy() const { return _py; }
@@ -165,8 +147,6 @@ namespace AcerDet {
 			void boostToRestFrame(ParticleT<T> &p);
 			void boostFromRestFrame(ParticleT<T> &p);
 		};
-
-		typedef ParticleT<Real64_t> Particle;
 
 		template<typename T>
 		inline double ParticleT<T>::getAnglePhi()
@@ -272,4 +252,3 @@ namespace AcerDet {
 }
 
 #endif
-
