594d13d7eff2d38b7611f86a4194dae958828268
Patryk Mikos <patryk912@gmail.com>
2014-07-25 20:27:20 +0200

    Muon dummy impl

--- src/analyse/Muon.cpp
+++ src/analyse/Muon.cpp
@@ -50,103 +50,95 @@ void Muon::printInfo() const {
 }
 
 void Muon::analyseRecord( const io::InputRecord& irecord, io::OutputRecord& orecord ) {
-	//printf ("Muon: analyse record\n");
-	/*
-      SUBROUTINE ACDMUO(MODE,LPAR,YPAR)
-
-      INTEGER  KEYHID, KEYSME, IEVENT 
-      INTEGER I, II, NSTART, NSTOP, IMUO, IMUOISO
-      REAL DDR, EDEP
-      REAL ANGLE
-      REAL ETA, PHI, PT
-      REAL RISOLJ, RDEP, EDMAX, PTMUMIN, ETCLU, PTMUMINT
-      REAL RCONE, ETAMAX
-      REAL RESMUO, SIGMA
-      REAL PXMUO, PYMUO, PZMUO, EEMUO
-      REAL ENER, JPT, JPHI, JETA
-      LOGICAL ISOL
-      INTEGER NBINA, IDENT
-      REAl TMAXA,TMINA
-      REAL YPAR
-      INTEGER MODE, LPAR 
-
-      ELSEIF(MODE.EQ.0) THEN
-C     ======================
-	IEVENT = IEVENT+1
-
-	// wyczysc tablice przed przetwarzaniem (nic nie rob)
-	NMUO = 0
-	DO I=1,100 {
-		KMUO(I,2)=0
-	}
-
-	NMUOX = 0
-	DO I=1,100 {
-		KMUOX(I,2) = 0
-	}
+	IEVENT++;
+	
+	const vector<Particle>& parts = irecord.particles();
 
 	// znajdz poczatek danych
-	NSTOP = 0
-	NSTART = 1
-	DO I=1, N {
-		IF(K(I,1) != 21) 
-		THEN
-			NSTOP = I-1
-			NSTART= I
-			break
-		ENDIF
+	Int32_t NSTOP = 0, NSTART = 1;
+	for (int i=0; i<parts.size(); ++i) {
+		if (parts[i].stateID != 21) {
+			NSTOP = i-1;
+			NSTART = i;
+			break;
+		}
 	}
 
-c....look for isolated muons, sort clusters common
-	DO I from NSTART to N {
-		IF(K(I,1) < 0 || K(I,1) > 10) continue
-		IF(ABS(K(I,2)) == 13) 					// czastka jest muonem
-		THEN
+	// look for isolated muons, sort clusters common
+	for (int i=NSTART; i<parts.size(); ++i) {
+		if (!parts[i].isStable()) 
+			continue;
 
-c -- analyse not decayed muons
-			ISOL = TRUE
-			PT = SQRT(P(I,1)**2+P(I,2)**2)
-			IF(PT < PTMUMINT) continue
-			ETA = SIGN(LOG((SQRT(PT**2+P(I,3)**2)+ABS(P(I,3)))/PT),P(I,3)) 
-			IF(ABS(ETA) > ETAMAX) continue  
-			PHI = ANGLE(P(I,1),P(I,2))
+		// it is muon
+		if (ABS(parts[i].typeID) == 13) {
+			
+			// analyse not decayed muons
+			ISOL = true;
+			
+			PT = parts[i].pT();
+			if (PT < PTMUMINT) 
+				continue;
+			
+			ETA = parts[i].getEta(); 
+			IF(ABS(ETA) > ETAMAX) 
+				continue;  
+			
+			PHI = parts[i].getPhi();
 
-c.... apply smearing
-			IF(KEYSME) 
-			THEN
+			// apply smearing
+			if (KEYSME) {
 				SIGMA = RESMUO(PT,ETA,PHI)
-				PXMUO = P(I,1) / (1.+SIGMA)
-				PYMUO = P(I,2) / (1.+SIGMA)
-				PZMUO = P(I,3) / (1.+SIGMA)
-				EEMUO = P(I,4) / (1.+SIGMA)
-				PT    = SQRT(PXMUO**2+PYMUO**2)
-			ENDIF
-			IF(PT < PTMUMIN) continue
-
-c ===   check is muon isolated from clusters
-			DO II from 1 to NCLU {
-				DDR = SQRT((ETA-PCLU(II,3))**2+(PHI-PCLU(II,4))**2)
-				IF(ABS(PHI-PCLU(II,4)) > PI)
-					DDR = SQRT( (ETA-PCLU(II,3))**2 + (ABS(PHI-PCLU(II,4))-2*PI)**2 )
-				IF(DDR < RISOLJ) 
-					ISOL = FALSE
+				PXMUO = parts[i].pX() / (1.0 + SIGMA);
+				PYMUO = parts[i].pY() / (1.0 + SIGMA);
+				PZMUO = parts[i].pZ() / (1.0 + SIGMA);
+				EEMUO = parts[i].e()  / (1.0 + SIGMA);
+				PT    = sqrt(PXMUO*PXMUO + PYMUO*PYMUO);
+			}
+			
+			if (PT < PTMUMIN) 
+				continue;
+			
+			// check if muon is isolated from clusters
+			for (int j=0; j<orecord.vCluster.size(); ++j) {
+				DDR = sqrt( 
+					pow(ETA-orecord.vCluster[j].P[2], 2.0) + 
+					pow(PHI-orecord.vCluster[j].P[3], 2.0) 
+				);
+				if (abs(PHI-orecord.vCluster[j].P[3]) > PI)
+					DDR = sqrt( 
+						pow(ETA-orecord.vCluster[j].P[2], 2.0) + 
+						pow(abs(PHI-orecord.vCluster[j].P[3])-2.0*PI, 2.0) 
+					);
+				if (DDR < RISOLJ) 
+					ISOL = false;
 			}
 
-c ===   check on energy deposition of   cells EDEP in cone RDEP
-			EDEP = 0.0
-			DO II from 1 to NCELL {
-				DDR = SQRT((ETA-PCELL(II,1))**2+(PHI-PCELL(II,2))**2)
-				IF(ABS(PHI-PCELL(II,2)) > PI)
-					DDR=SQRT( (ETA-PCELL(II,1))**2 + (ABS(PHI-PCELL(II,2))-2*PI)**2 )
-				IF(DDR < RDEP) 
-					EDEP = EDEP + PCELL(II,5)
+			// check on energy deposition of cells EDEP in cone RDEP
+			Real64_t EDEP = 0.0;
+			for (int j=0; j<orecord.vCell.size(); ++j) {
+				DDR = sqrt(
+					pow(ETA - orecord.vCell[j].P[0], 2.0) +
+					pow(PHI - orecord.vCell[j].P[1], 2.0)
+				);
+				
+				if (abs(PHI - orecord.vCell[j].P[1]) > PI)
+					DDR = sqrt( 
+						pow(ETA - orecord.vCell[j].P[0], 2) + 
+						pow(abs(PHI - orecord.vCell[j].P[1]) - 2*PI, 2) 
+					);
+				
+				if (DDR < RDEP) 
+					EDEP += orecord.vCell[j].P[4];
+		
+		
+		
 			}
-			IF(EDEP > EDMAX) 
-				ISOL = FALSE
-
-c ===   fill /ISOMUO/ with isolated muon
-			IF(ISOL) 
-			THEN
+			
+			if (EDEP > EDMAX) 
+				ISOL = false;
+// PARTICLE STRUCTURE				
+			// fill /ISOMUO/ with isolated muon
+			if (ISOL) {
 				NMUO = NMUO + 1
 				KMUO(NMUO,1) = NMUO
 				KMUO(NMUO,2) = K(I,2)
@@ -159,86 +151,72 @@ c ===   fill /ISOMUO/ with isolated muon
 				PMUO(NMUO,3) = ETA
 				PMUO(NMUO,4) = PHI
 				PMUO(NMUO,5) = PT
-			ELSE
-c ===   fill /NOISOMUO/ with non-isolated muon
+			} else {
+			// fill /NOISOMUO/ with non-isolated muon
 				NMUOX = NMUOX + 1
 				KMUOX(NMUOX,1) = NMUOX
 				KMUOX(NMUOX,2) = K(I,2)
-          			KMUOX(NMUOX,3) = I
+          		KMUOX(NMUOX,3) = I
 				KMUOX(NMUOX,4) = K(K(I,3),2)
 				KMUOX(NMUOX,5) = 1
 
-          			PMUOX(NMUOX,1) = ETA
-          			PMUOX(NMUOX,2) = PHI
-          			PMUOX(NMUOX,3) = ETA
-          			PMUOX(NMUOX,4) = PHI
-          			PMUOX(NMUOX,5) = PT
-			ENDIF //isol
-		ENDIF //is muon
-c.............
-	} // end for
-
-	CALL HF1(IDENT+11,REAl(NMUO),1.0)
-	CALL HF1(IDENT+10,REAl(NMUOX),1.0)
-
-c....cross-check with partons
-	IMUO = 0
-	IMUOISO = 0
-	DO I from 1 to NSTOP {
-		IF(ABS(K(I,2)) == 13) {
-			PT = SQRT(P(I,1)**2+P(I,2)**2) 
-			ETA = SIGN(LOG((SQRT(PT**2+P(I,3)**2)+ABS(P(I,3)))/PT),P(I,3)) 
-			PHI = ANGLE(P(I,1),P(I,2))
-			ENER = 0.0
-			ISOL = TRUE
+          		PMUOX(NMUOX,1) = ETA
+          		PMUOX(NMUOX,2) = PHI
+          		PMUOX(NMUOX,3) = ETA
+          		PMUOX(NMUOX,4) = PHI
+          		PMUOX(NMUOX,5) = PT
+			}
+		}
+	}
+	
+	// save to histogram
+	// CALL HF1(IDENT+11, REAl(NMUO), 1.0);
+	// CALL HF1(IDENT+10, REAl(NMUOX), 1.0);
+	
+	// cross-check with partons
+	Int32_t IMUO = 0, IMUOISO = 0;
+	for (int i=0; i<=NSTOP; ++i) {
+		if (abs(parts[i].typeID) == 13) { // is muon
+			PT = parts[i].pT();
+			ETA = parts[i].getEta(); 
+			PHI = parts[i].getPhi();
+			ENER = 0.0;
+			ISOL = true;
 			
-			DO II from 1 to NSTOP {
-				IF(ABS(K(II,2)) <= 21 && I != II && ABS(K(II,2)) != 12 && ABS(K(II,2)) != 14 && ABS(K(II,2)) != 16) {
-					JPT = SQRT(P(II,1)**2+P(II,2)**2) 
-					JETA = SIGN(LOG((SQRT(JPT**2+P(II,3)**2)+ABS(P(II,3)))/JPT),P(II,3)) 
-					JPHI = ANGLE(P(II,1),P(II,2))
-					DDR = SQRT((ETA-JETA)**2 + (JPHI-PHI)**2)
-					IF(ABS(JPHI-PHI) > PI)
-						DDR = SQRT( (ETA-JETA)**2 + (ABS(JPHI-PHI)-2*PI)**2 )
-					IF(DDR < RISOLJ && JPT > ETCLU) 
-						ISOL = FALSE
-					IF(DDR < RDEP && JPT < ETCLU) 
-						ENER = ENER + JPT
+			for (int j=0; j<=NSTOP; ++j) {
+				if (abs(parts[j].typeID) <= 21 && i != j && 
+					abs(parts[j].typeID) != 12 && 
+					abs(parts[j].typeID) != 14 && 
+					abs(parts[j].typeID) != 16) 
+				{
+					JPT = part[j].pT(); 
+					JETA = part[j].getEta(); 
+					JPHI = part[j].getPhi();
+					
+					DDR = sqrt( pow(ETA-JETA, 2) + pow(JPHI-PHI, 2) );
+					if (abs(JPHI-PHI) > PI)
+						DDR = sqrt( pow(ETA-JETA, 2) + pow(abs(JPHI-PHI)-2*PI, 2) );
+
+					if (DDR < RISOLJ && JPT > ETCLU) 
+						ISOL = false;
+						
+					if (DDR < RDEP && JPT < ETCLU) 
+						ENER += JPT;
 				}
 			}
 
-			IF(ENER > EDMAX) 
-				ISOL = FALSE
-			IF(ABS(ETA) < ETAMAX && PT > PTMUMIN) 
-				IMUO = IMUO + 1
-			IF(ABS(ETA) < ETAMAX && PT > PTMUMIN && ISOL)
-				IMUOISO = IMUOISO + 1 
+			if (ENER > EDMAX) 
+				ISOL = false;
+				
+			if (abs(ETA) < ETAMAX && PT > PTMUMIN) 
+				IMUO++;
+				
+			if (abs(ETA) < ETAMAX && PT > PTMUMIN && ISOL)
+				IMUOISO++;
 		}
 	}
 
-	CALL HF1(IDENT+21,REAL(IMUO),1.0)
-	CALL HF1(IDENT+31,REAL(IMUOISO),1.0)
-
-C
-      ELSEIF(MODE.EQ.1) THEN
-C     =========================
-
-      WRITE(NOUT,BXOPE)
-      WRITE(NOUT,BXTXT) '*********************************'
-      WRITE(NOUT,BXTXT) '        OUTPUT FROM AcerDET      '
-      WRITE(NOUT,BXTXT) '              ACDMUO             '
-      WRITE(NOUT,BXTXT) '*********************************'
-      WRITE(NOUT,BXCLO)
-
-      CALL HPRINT(IDENT+10)
-      CALL HPRINT(IDENT+11)
-      CALL HPRINT(IDENT+21)
-      CALL HPRINT(IDENT+31)
-
-      ENDIF
-C     =====
-
-      END
-*/
+	// CALL HF1(IDENT+21,REAL(IMUO),1.0)
+	// CALL HF1(IDENT+31,REAL(IMUOISO),1.0)
 
 }
