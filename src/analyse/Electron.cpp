41b6ea40b27b023e5f5a986aba2ec63b632f4230
Patryk Mikos <patryk912@gmail.com>
2014-07-25 20:27:31 +0200

    Electron dummy impl

--- src/analyse/Electron.cpp
+++ src/analyse/Electron.cpp
@@ -50,136 +50,132 @@ void Electron::printInfo() const {
 }
 
 void Electron::analyseRecord( const io::InputRecord& irecord, io::OutputRecord& orecord ) {
-	//printf ("Electron: analyse record\n");
-	/*
-      SUBROUTINE ACDELE(MODE,LPAR,YPAR)
-
-      INTEGER I, II, III, NSTART, NSTOP, IELE, IELEISO
-      REAL  DR, DDR, EDEP
-      REAL ANGLE
-      REAL ETA, PHI, PT, PTCRU
-      REAL RJE, RISOLJ, RDEP, EDMAX, PTLMIN, ETAMAX, ETCLU
-      REAL RCONE
-      REAL ENER, JPT, JPHI, JETA
-      REAL PXELE, PYELE, PZELE, EEELE
-      REAL RESELE, SIGPH, ENE
-      INTEGER KDUM,IDU,IMU, IMAX
-      REAL PDUM, ETMAX
-      INTEGER LCLU
-      LOGICAL ISOL
-      INTEGER NBINA, IDENT
-      REAL TMAXA,TMINA
-
-// Analiza przypadku
-      ELSEIF(MODE.EQ.0.OR.MODE.EQ.2) THEN
-C     ===================================
-      IEVENT = IEVENT+1
-
-
-      NELE=0     // liczba znalezionych elektronow w tym obiegu
-      DO I=1,100
-        KELE(I,2)=0  // macierz K - na flagi    P - na zmienne momenty pedy energie (real)
-      ENDDO
-
-// wyznacz koniec historii i poczatek fianl state particle
-// analiza dwoch kawalkow eventu osobno
-	NSTOP = 0  
-	NSTART = 1
-	DO I from 1 to N {
-		IF(K(I,1) != 21) { 				// K(1) == 21 -> status historia
-			NSTOP = I - 1
-			NSTART = I
-			break
+	IEVENT++;
+	
+	const vector<Particle>& parts;
+	
+	Int32_t NSTOP = 0, NSTART = 1;
+	for (int i=0; i<parts.size(); ++i) {
+		if (parts[i].statusID != 21) {
+			NSTOP = i-1;
+			NSTART = i;
+			break;
 		}
 	}
+	
+	// look for isolated electrons, sort clusters common
+	for (int i=NSTART; i<parts.size(); ++i) {
+		if (!parts[i].isStable()) 
+			continue;
+			
+		// analyse electrons
+		if (abs(parts[i].typeID) == 11) {
+			ISOL = true;
+			LCLU = 0;
+			PT = parts[i].pT();
+        	ETA = parts[i].getEta();
+        	PHI = parts[i].getPhi();
+   
+			// apply smearing
+        	PTCRU = PT;
+			if (KEYSME) {
+				ENE = parts[i].e();
+				if (ENE <= 0.0) 
+					continue;
 
-c....look for isolated electrons, sort clusters common
-// w czesci drugiej
-// K(1) status czastki
-	DO I from NSTART to N {
-		IF(K(I,1) < 0 || K(I,1) > 10) 
-			continue   				// tylko dla czastek ze statusem 1 do 10
-
-c -- analyse electrons
-		IF(ABS(K(I,2)) == 11) {    			// K(2) typ czastki +- 11
-			ISOL  = TRUE  		        	// izolowany
-			LCLU  = 0
-			PT = SQRT(P(I,1)**2+P(I,2)**2)   	// zmienne kinematyczne
-        		ETA = SIGN(LOG((SQRT(PT**2+P(I,3)**2)+ABS(P(I,3)))/PT),P(I,3))
-        		PHI = ANGLE(P(I,1),P(I,2))
-        		PTCRU = PT  				// wartosc pedu poprzecznego przed rozmazywaniem
-c.....apply smearing
-			IF(KEYSME) {          			// jesli ustawiona flaga  rozmaz wg gaussa
-				ENE = P(I,4)
-				IF(ENE <= 0.0) 
-					contninue
-
-				SIGPH = RESELE(ENE,PT,ETA,PHI)
-				PXELE = P(I,1) * (1.+SIGPH)
-				PYELE = P(I,2) * (1.+SIGPH)
-				PZELE = P(I,3) * (1.+SIGPH)
-				EEELE = P(I,4) * (1.+SIGPH)
-				PT  = SQRT(PXELE**2+PYELE**2)    	// jescze raz wylicz
+				SIGPH = RESELE(ENE,PT,ETA,PHI);
+				PXELE = P(I,1) * (1.0 + SIGPH);
+				PYELE = P(I,2) * (1.0 + SIGPH);
+				PZELE = P(I,3) * (1.0 + SIGPH);
+				EEELE = P(I,4) * (1.0 + SIGPH);
+				PT = sqrt(PXELE*PXELE + PYELE*PYELE);
 			}
         
-			IF(PT < PTLMIN)              			// stala z konfiga
-				contninue
-			IF(ABS(ETA) > ETAMAX)			        // stala z konf
-				contninue
+			if (PT < PTLMIN)
+				continue;
 
-c === mark eletron-cluster              			        // klastry powiny byc wczesniej zrekonstruowane (powinien byc mapping kluster -elektron)
-			DR = 100.0					// znajdz kluster odpowiadajacy elektronowi jako rozwartosc katowa  PCLU - zrekonstruowane klastry
-			DO II from 1 to NCLU {				// klastry odpowiadaja rejonom kalorymetry
-				DDR = SQRT((ETA-PCLU(II,3))**2+(PHI-PCLU(II,4))**2)
-				IF(ABS(PHI-PCLU(II,4)) > PI)
-					DDR = SQRT(  (ETA-PCLU(II,3))**2 + (ABS(PHI-PCLU(II,4))-2*PI)**2 )
-				IF(DDR < DR) LCLU = II
-				DR = MIN(DDR,DR)			// minimalna odleglosc
+			if (abs(ETA) > ETAMAX)
+				continue;
+				
+			// mark eletron-cluster
+			DR = 100.0;
+			for (int j=0; j<orecord.vCluster.size(); ++j) {
+				DDR = sqrt(
+					pow(ETA - orecord.vCluster[j].P[2], 2) +
+					pow(PHI - orecord.vCluster[j].P[3], 2)
+				);
+				
+				if (abs(PHI - orecord.vCluster[j].P[3]) > PI)
+					DDR = sqrt( 
+						pow(ETA - orecord.vCluster[j].P[2], 2) + 
+						pow(abs(PHI - orecord.vCluster[j].P[3]) - 2*PI), 2)
+					);
+					
+				if (DDR < DR) 
+					LCLU = j;
+					
+				DR = MIN(DDR,DR);
 			}
 
-			IF(DR > RJE) 
-				LCLU = 0 				// jesli nie ma klustra w granicach konfiguracji to oznacz jako poza ( nie dotarl do kalorymetru )
+			if (DR > RJE) 
+				LCLU = -1;
 
-c === check if electron  isolated from clusters
-			DO II from 1 to NCLU {				// szukaj drugiego najblizszego i sprawdz czy sie miesci w granicach
-				IF(II == LCLU) {
-					DDR = 100.0
-				} ELSE {
-					DDR = SQRT((ETA-PCLU(II,3))**2+(PHI-PCLU(II,4))**2)
-					IF(ABS(PHI-PCLU(II,4)) > PI)
-						DDR = SQRT(  (ETA-PCLU(II,3))**2 + (ABS(PHI-PCLU(II,4))-2*PI)**2 )
+			// check if electron isolated from clusters
+			for (int j=0; j<orecord.vCluster.size(); ++j) {
+				if (j == LCLU) {
+					DDR = 100.0;
+				} else {
+					DDR = sqrt(
+						pow(ETA - orecord.vCluster[j].P[2], 2) + 
+						pow(PHI - orecord.vCluster[j].P[3], 2)
+					);
+					
+					if (abs(PHI - orecord.vCluster[j].P[3]) > PI)
+						DDR = sqrt(
+							pow(ETA - orecord.vCluster[j].P[2], 2) + 
+							pow(abs(PHI - orecord.vCluster.P[3]) - 2*PI, 2)
+						);
 				}
 
-				IF(DDR < RISOLJ) 
-					ISOL = FALSE
+				if (DDR < RISOLJ) 
+					ISOL = false;
+			}
+			
+			// check on energy deposition of cells EDEP in cone RDEP
+			EDEP = 0.0;
+			for (int j=0; j<orecord.vCell.size(); ++j) {
+				DDR = sqrt(
+					pow(ETA - orecord.vCell[j].P[0], 2) + 
+					pow(PHI - orecord.vCell[j].P[1], 2)
+				);
+				
+				if (abs(PHI - orecord.vCell[j].P[1]) > PI)
+					DDR = sqrt(
+						pow(ETA - orecord.vCell[j].P[0], 2) + 
+						pow(abs(PHI - orecord.vCell[j].P[1]) - 2*PI, 2)
+					);
+					
+				if (DDR < RDEP) 
+					EDEP += orecord.vCell[j].P[4];
 			}
 
-c ===   check on energy deposition of   cells EDEP in cone RDEP		// kluster to grupka celli  cell to fragment kalorymetru
-			EDEP = 0.0					// szukaj celli nie uzytych do klastrow
-			DO II from 1 to NCELL {
-				DDR = SQRT((ETA-PCELL(II,1))**2+(PHI-PCELL(II,2))**2)
-				IF(ABS(PHI-PCELL(II,2)) > PI)
-					DDR = SQRT(  (ETA-PCELL(II,1))**2 + (ABS(PHI-PCELL(II,2))-2*PI)**2 )
-				IF(DDR < RDEP) 
-					EDEP += PCELL(II,5)
-			}
-
-			IF(EDEP-PTCRU > EDMAX) 
-				ISOL = FALSE
-
-c ===  fill /ISOELE/ with isolated electron 
-c ===  remove ele-cluster from /CLUSTER/
-			IF(ISOL) {						// jest izolowany i od clustrow i od cell wiec laduje na kolekcji
-				NELE = NELE + 1
-				IF(LCLU != 0) { 			// z listy klustrow usuwamy ten ktory byl z nim stowarzyszony
-					DO II from LCLU to NCLU-1 {			// i zapamietaj jego
+			if (EDEP - PTCRU > EDMAX) 
+				ISOL = false;
+				
+			// fill /ISOELE/ with isolated electron 
+			// remove ele-cluster from /CLUSTER/
+			if (ISOL) {
+				/* usun stowarzyszony cluster z listy clustrow
+				if (LCLU != -1) {
+					DO II from LCLU to NCLU-1 {
 						DO III from 1 to 5 {
-							KCLU(II,III) = KCLU(II+1,III)       // usun i przesun tablice nadpisujac 
-                   					PCLU(II,III) = PCLU(II+1,III)
-                 				}
+							KCLU(II,III) = KCLU(II+1,III) 
+                   			PCLU(II,III) = PCLU(II+1,III)
+                 		}
 					}
 					NCLU = NCLU-1
 				}
+				*/
 
 				KELE(NELE,1) = NELE
 				KELE(NELE,2) = K(I,2)			// typ elktronu
@@ -189,18 +185,17 @@ c ===  remove ele-cluster from /CLUSTER/
 
 				PELE(NELE,1) = ETA
 				PELE(NELE,2) = PHI
-           			PELE(NELE,3) = ETA
-           			PELE(NELE,4) = PHI
-           			PELE(NELE,5) = PT
+           		PELE(NELE,3) = ETA
+           		PELE(NELE,4) = PHI
+           		PELE(NELE,5) = PT
 			}
 		}
 	}
 
-// dane do histogramu
-	CALL HF1(IDENT+11, REAL(NELE), 1.0)
+	// CALL HF1(IDENT+11, REAL(NELE), 1.0)
 
-c.....arrange electrons in falling E_T sequence			// sort by E_T
-	DO IDU from 1 to NELE {					// naiwne sortowanie przez wybor maxa, wykorzystuje poczatkowy fragment dum jako tmp tablice
+	// arrange electrons in falling E_T sequence
+	DO IDU from 1 to NELE {
 		ETMAX = 0
 		DO IMU from 1 to NELE {
 			IF(KELE(IMU,5) == 0) continue
@@ -227,72 +222,57 @@ c.....arrange electrons in falling E_T sequence			// sort by E_T
 
 
 // dla czastek z histori
-	IELE    = 0
-	IELEISO = 0
+	Int32_t IELE = 0;
+	Int32_t IELEISO = 0;
 
-	DO I from 1 to NSTOP {
-		IF(ABS(K(I,2)) == 11) {
-			PT = SQRT(P(I,1)**2+P(I,2)**2) 
-			ETA = SIGN(LOG((SQRT(PT**2+P(I,3)**2)+ABS(P(I,3)))/PT),P(I,3)) 
-			PHI = ANGLE(P(I,1),P(I,2))
-			ENER = 0.0
-			ISOL = TRUE
+	for (int i=0; i<=NSTOP; ++i) {
+		if (abs (parts[i].typeID) == 11) {
+			PT = parts[i].pT(); 
+			ETA = parts[i].getEta(); 
+			PHI = parts[i].getPhi();
+			ENER = 0.0;
+			ISOL = true;
 
-			DO II from 1 to NSTOP {
-				IF(ABS(K(II,2)) <= 21 && I != II && ABS(K(II,2)) != 12 && ABS(K(II,2)) != 14 && ABS(K(II,2)) != 16) {
+			for (int j=0; j<=NSTOP; ++j) {
+				if (abs(parts[j].typeID) <= 21 && i != j && 
+					abs(parts[j].partID) != 12 && 
+					abs(parts[j].partID) != 14 && 
+					abs(parts[j].partID) != 16) 
+				{
 					// przelicz dane dla II czastki - standard per particle
-					JPT = SQRT(P(II,1)**2+P(II,2)**2) 
-					JETA = SIGN(LOG((SQRT(JPT**2+P(II,3)**2)+ABS(P(II,3)))/JPT),P(II,3)) 
-					JPHI = ANGLE(P(II,1),P(II,2))
-					DDR = SQRT((ETA-JETA)**2+(JPHI-PHI)**2)
+					JPT = parts[j].pT(); 
+					JETA = parts[j].getEta(); 
+					JPHI = parts[j].getPhi();
+					DDR = sqrt(
+						pow(ETA-JETA, 2) + 
+						pow(JPHI-PHI, 2)
+					);
 
-					IF(ABS(JPHI-PHI) > PI)
-						DDR = SQRT((ETA-JETA)**2 + (ABS(JPHI-PHI)-2*PI)**2 )
+					if (abs(JPHI-PHI) > PI)
+						DDR = sqrt(
+							pow(ETA-JETA, 2) + 
+							pow(abs(JPHI-PHI)-2*PI), 2)
+						);
 
-					IF(DDR < RISOLJ && JPT > ETCLU) 
-						ISOL = FALSE
+					if (DDR < RISOLJ && JPT > ETCLU) 
+						ISOL = false;
 
-					IF(DDR < RDEP && JPT < ETCLU) 
-						ENER += JPT
+					if (DDR < RDEP && JPT < ETCLU) 
+						ENER += JPT;
 				}
 			}
 
-			IF(ENER > EDMAX) 
-				ISOL = FALSE
+			if (ENER > EDMAX) 
+				ISOL = false;
 
-			IF(ABS(ETA) < ETAMAX && PT > PTLMIN)
-				IELE = IELE + 1
+			if (abs(ETA) < ETAMAX && PT > PTLMIN)
+				IELE++;
 
-			IF(ABS(ETA) < ETAMAX && PT > PTLMIN && ISOL)
-				IELEISO = IELEISO + 1
+			if (abs(ETA) < ETAMAX && PT > PTLMIN && ISOL)
+				IELEISO++;
 		}
 	}
-
-// ile takich ele bylo w tej czesci i ile bylo izolowanych (check na tym co robilimsy wczesniej) 
-         CALL HF1(IDENT+21, REAL(IELE), 1.0)
-         CALL HF1(IDENT+31, REAL(IELEISO), 1.0)
-
-C
-      ELSEIF(MODE.EQ.1) THEN
-C     =======================
-
-// druk histogramow
-
-      WRITE(NOUT,BXOPE)
-      WRITE(NOUT,BXTXT) '*********************************'
-      WRITE(NOUT,BXTXT) '        OUTPUT FROM AcerDET      '
-      WRITE(NOUT,BXTXT) '              ACDELE             '
-      WRITE(NOUT,BXTXT) '*********************************'
-      WRITE(NOUT,BXCLO)
-
-      CALL HPRINT(IDENT+11)
-      CALL HPRINT(IDENT+21)
-      CALL HPRINT(IDENT+31)
-
-      ENDIF
-C     =====
-
-      END
-*/
-
+	
+	// CALL HF1(IDENT+21, REAL(IELE), 1.0)
+	// CALL HF1(IDENT+31, REAL(IELEISO), 1.0)
 }
