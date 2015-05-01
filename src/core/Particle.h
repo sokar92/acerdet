f19a5e549f6c89db4efbb63a3eaea146f54a9304
Patryk Mikos <usciskajkaktusa@gmail.com>
2015-05-01 15:50:57 +0200

    Comments: Particle.h fully commented

--- src/core/Particle.h
+++ src/core/Particle.h
@@ -17,97 +17,99 @@ using namespace std;
 namespace AcerDet {
 	namespace core {
 		
-		/*
-		  A state of particle
+		/**
+		  A current state of particle
 		*/
 		enum ParticleStatus {
-			PS_NULL = 0, /*!< detailed description  */
-			PS_BEAM, /*!< detailed description  */
-			PS_FINAL, /*!< detailed description  */
-			PS_DECAYED, /*!< detailed description  */
-			PS_HISTORY, /*!< detailed description  */
-			PS_CASCADE_QUARK, /*!< b or c quark not from hard process */
-			PS_HP_QUARK /*!< b or c quark from hard process */
+			PS_NULL = 0,      /*!< Undefined Particle state. Should not occur in valid program. */
+			PS_BEAM,          /*!< TODO: detailed description  */
+			PS_FINAL,         /*!< TODO: detailed description  */
+			PS_DECAYED,       /*!< TODO: detailed description  */
+			PS_HISTORY,       /*!< TODO: detailed description  */
+			PS_CASCADE_QUARK, /*!< b or c quark outside of hard process. */
+			PS_HP_QUARK       /*!< b or c quark from hard process. */
 		};
 		
-		/*
-		  Type of particle - self descriptive
+		/**
+		  Type of particle
 		*/
 		enum ParticleType {
-			PT_UNKNOWN = 0,	/*!< detailed description  */
+			PT_UNKNOWN = 0,            /*!< Unknown Particle type. If this type occurs in some particle check your algorithm! */
 			
-			PT_JET,			/*!< detailed description  */
-			PT_BJET,		/*!< detailed description  */	// 5
-			PT_CJET,		/*!< detailed description  */	// 4
+			PT_JET,                    /*!< Parton representing Jet of grouped particles. */
+			PT_BJET,         /* 5 */   /*!< Parton representing a Jet of b particles. */
+			PT_CJET,         /* 4 */   /*!< Parton representing a Jet of c particles. */
 			
-			PT_CELL,		/*!< detailed description  */
-			PT_CLUSTER,		/*!< detailed description  */
+			PT_CELL,                   /*!< Grouping parton representing a single Cell in detector. */
+			PT_CLUSTER,                /*!< Grouping parton representing a Cluster aka. collection of consecutive Cells in detector. */
 			
-			PT_MUON,		/*!< muon */					// 13
-			PT_ELECTRON,	/*!< electron/positron e-/e+ */	// 11
-			PT_PHOTON,		/*!< gamma */					// 22 (gamma)
-			PT_TAU,			/*!< tau */						// 15
+			PT_MUON,         /* 13 */  /*!< A muon particle. */
+			PT_ELECTRON,     /* 11 */  /*!< An electron or positron (e- / e+). */
+			PT_PHOTON,       /* 22 */  /*!< Nonmassive gamma particle. */
+			PT_TAU,          /* 15 */  /*!< A tau particle. */
 			
-			PT_NEUTRINO_ELE, /*!< electron neutrino */		// 12
-			PT_NEUTRINO_MUO, /*!< muon neutrino */			// 14
-			PT_NEUTRINO_TAU, /*!< tau neutrion */			// 16
+			PT_NEUTRINO_ELE, /* 12 */  /*!< An electron neutrino. */
+			PT_NEUTRINO_MUO, /* 14 */  /*!< A muon neutrino. */
+			PT_NEUTRINO_TAU, /* 16 */  /*!< A tau neutrino. */
 			
-			PT_BOSON_Z, /*!< Z0 boson */					// 23
-			PT_BOSON_W, /*!< W+ boson */					// 24
-			PT_BOSON_H  /*!< Higgs boson */					// 25
+			PT_BOSON_Z,      /* 23 */  /*!< Z0 boson. */
+			PT_BOSON_W,      /* 24 */  /*!< W+ boson. */
+			PT_BOSON_H       /* 25 */  /*!< Higgs boson. */
 		};
 		
-		/*
-		 * Common class representing partons
+		/**
+		 * Common class representing partons.
+		 * Stores it's type, status, four-vectors representing momentum and production vertex (if exists), mother and daughters ids.
 		 */
 		class Particle {
 		public:
-			ParticleStatus status; /*!< particle status converted to enum */
-			Int32_t statusID; /*!< particle status */
+			ParticleStatus status; /*!< Enumerated particle status. */
+			Int32_t statusID;      /*!< Original not converted particle status (comes from generator). */
 
-			ParticleType type; /*!< pdg_id converted to enum */
-			Int32_t pdg_id; /*!< pdg_id */
+			ParticleType type;     /*!< Enumerated particle type. (converted from particle PDGid). */
+			Int32_t pdg_id;        /*!< Original not converted particle PDGid. (global convention). */
 			
-			Vector4f momentum; /*!< momentum */
-			Vector4f production; /*!< production vertex if exists */
+			Vector4f momentum;     /*!< A four-vector representing total particle momentum. */
+			Vector4f production;   /*!< A four-vector representing particle production vertex coordinates (if such vertex exists). */
 
-			Int32_t barcode; /*!< particle barcode (id in sequence [1...n]) */
-			Int32_t mother; /*!< mother barcode if exists, -1 otherwise */
-			pair<Int32_t,Int32_t> daughters; /*!< pair of daughter barcodes, <-1,-1> otherwise */
+			Int32_t barcode;       /*!< Particle barcode. A unique id representing particle. */
+			Int32_t mother;        /*!< If particle has mother, mother's barcode. Otherwise -1.*/
+			pair<Int32_t,Int32_t> daughters; /*!< If particle has daughters, a range of daughter barcodes. Otherwise <-1,-1>. */
 
-			/*
-			 * Default ctor
-			 * Initialize particle data
+			/**
+			 * Default particle constructor.
+			 * Initialize particle data.
 			 */
 			Particle();
 
-			/*
-			 * Check if given particle has mother
+			/**
+			 * Check if given particle has mother.
 			 */
 			Bool_t hasMother() const;
 
-			/*
-			 * Check if given particle has any daughters
+			/**
+			 * Check if given particle has any daughters.
 			 */
 			Bool_t hasDaughter() const;
 
-			/*
-			 * Number of particle daughters
+			/**
+			 * Number of particle daughters.
 			 */
 			Int32_t daughtersCount() const;
 
-			/*
-			 * Check if particle is beam
+			/**
+			 * Check if given particle is beam.
 			 */
 			Bool_t isBeam() const;
 			
-			/*
-			 * Type Check
+			/**
+			 * Check if given particle is a nautrino.
 			 */
 			Bool_t isNeutrino() const;
 
-			/*
-			* Print basic informations about particle
+			/**
+			* Print out to std::ostream basic information about given particle.
+			* Prints out barcode, type, status, momentum and production vertex.
 			*/			
 			friend ostream& operator << (ostream& str, const Particle& p) {
 				str << "Particle:" << endl;
@@ -120,30 +122,50 @@ namespace AcerDet {
 				return str;
 			}
 			
-			/*
-			 * useful
+			/**
+			 * Length of momentum four-vector projected on XY plane.
+			 * Returns sqrt(x*x + y*y), where [x,y,z,e] is a particle momentum.
 			 */
 			Real64_t pT() const;
 			
-			/*
-			 * Momentum short-linked accessors
+			/**
+			 * Particle momentum X-coordinate, where [x,y,z,e] is a particle momentum.
 			 */
 			inline Real64_t pX() const { return momentum.p.x; }
+			
+			/**
+			 * Particle momentum Y-coordinate, where [x,y,z,e] is a particle momentum.
+			 */
 			inline Real64_t pY() const { return momentum.p.y; }
+			
+			/**
+			 * Particle momentum Z-coordinate, where [x,y,z,e] is a particle momentum.
+			 */
 			inline Real64_t pZ() const { return momentum.p.z; }
+			
+			/**
+			 * Particle momentum E-coordinate, where [x,y,z,e] is a particle momentum.
+			 */
 			inline Real64_t e() const { return momentum.e; }
 			
-			/*
-			 * Angle in XY plane
+			/**
+			 * Particle momentum phi angle after projection on XY plane.
 			 */
 			Real64_t getPhi() const;
 			
+			/**
+			 * Particle momentum eta angle (as a logarithm).
+			 */
 			Real64_t getEta() const;
 			
+			/**
+			 * Particle momentum theta angle.
+			 */
 			Real64_t getTheta() const;
 			
-			/*
-			 * Folding
+			/**
+			 * Particle momentum phi angle folding.
+			 * Returns 0.5 / getPhi()
 			 */
 			Real64_t foldPhi() const;
 
@@ -152,133 +174,6 @@ namespace AcerDet {
 			
 			string getStatusName() const;
 		};
-
-		/*
-		  Single particle class with basic methods such as boost, rotation,
-		  and angle calculation.
-		*/
-
-/*
-		public:
-			// Invariant mass. If mass is negative then -sqrt(-mass) is returned
-			T recalculated_mass()  const
-			{
-				T p2 = _px*_px + _py*_py + _pz*_pz;
-				T e2 = _e*_e;
-				T m2 = e2 - p2;
-
-				if ( m2 < 0.0 ) return -sqrt( -m2 );
-				return sqrt( m2 );
-			}
-
-		public:
-			void rotateXZ(double theta);
-			void rotateXY(double theta);
-			void boostAlongZ(double pz, double e);
-			void boostToRestFrame(ParticleT<T> &p);
-			void boostFromRestFrame(ParticleT<T> &p);
-		};
-
-		template<typename T>
-		inline double ParticleT<T>::getAnglePhi()
-		{
-			// conventions as in ANGFI(X,Y) of tauola.f and PHOAN1 of photos.f
-			// but now 'phi' in name define that it is rotation in px py
-
-			double buf = 0.;
-
-			if ( fabs(getPy()) < fabs(getPx()) ) {
-				buf = atan( fabs(getPy()/getPx()) );
-				if (getPx() < 0.) buf = M_PI - buf;
-			} else buf = acos( getPx()/sqrt(getPx()*getPx() + getPy()*getPy()) );
-
-			if ( getPy() < 0. ) buf = 2.*M_PI - buf;
-			return buf;
-		}
-
-		template<typename T>
-		inline double ParticleT<T>::getAngleTheta()
-		{
-			// conventions as in ANGXY(X,Y) of tauola.f or PHOAN2 of photos.f
-			// but now 'theta' in name define that it is rotation in pz px
-			// note that first argument of PHOAN2 was usually z
-
-			double buf = 0.;
-
-			if ( fabs(getPx()) < fabs(getPz()) ) {
-				buf = atan( fabs(getPx()/getPz()) );
-				if ( getPz() < 0. ) buf = M_PI - buf;
-			}
-			else buf = acos( getPz()/sqrt(getPz()*getPz() + getPx()*getPx()) );
-			return buf;
-		}
-
-		template<typename T>
-		inline void ParticleT<T>::rotateXZ(double theta)
-		{
-			// as PHORO2 of photos.f
-			T pX = getPx();
-			T pZ = getPz();
-			setPx( cos(theta)*pX + sin(theta)*pZ);
-			setPz(-sin(theta)*pX + cos(theta)*pZ);
-		}
-
-		template<typename T>
-		inline void ParticleT<T>::rotateXY(double theta)
-		{
-			// as PHORO3 of photos.f
-			T pX = getPx();
-			T pY = getPy();
-			setPx( cos(theta)*pX - sin(theta)*pY);
-			setPy( sin(theta)*pX + cos(theta)*pY);
-		}
-
-		template<typename T>
-		inline void ParticleT<T>::boostAlongZ(double p_pz, double p_e)
-		{
-			// as PHOBO3 of photos.f
-			double m = sqrt(p_e*p_e-p_pz*p_pz);
-			T buf_pz = getPz();
-			T buf_e = getE();
-
-			setPz((p_e *buf_pz + p_pz*buf_e)/m);
-			setE ((p_pz*buf_pz + p_e *buf_e)/m);
-		}
-
-		template<typename T>
-		inline void ParticleT<T>::boostToRestFrame(ParticleT<T> &p)
-		{
-			T p_len = sqrt(p.getPx()*p.getPx() + p.getPy()*p.getPy() + p.getPz()*p.getPz());
-			double phi   = p.getAnglePhi();
-			p.rotateXY(-phi );
-			double theta = p.getAngleTheta();
-			p.rotateXY( phi );
-
-			//Now rotate coordinates to get boost in Z direction.
-			rotateXY(-phi  );
-			rotateXZ(-theta);
-			boostAlongZ(-1*p_len, p.getE());
-			rotateXZ( theta);
-			rotateXY( phi  );
-		}
-
-		template<typename T>
-		inline void ParticleT<T>::boostFromRestFrame(ParticleT<T> &p)
-		{
-			T p_len = sqrt(p.getPx()*p.getPx() + p.getPy()*p.getPy() + p.getPz()*p.getPz());
-			double phi   = p.getAnglePhi();
-			p.rotateXY(-phi );
-			double theta = p.getAngleTheta();
-			p.rotateXY( phi );
-
-			//Now rotate coordinates to get boost in Z direction.
-			rotateXY(-phi  );
-			rotateXZ(-theta);
-			boostAlongZ(p_len, p.getE());
-			rotateXZ( theta);
-			rotateXY( phi  );
-		}
-*/
 	}
 }
 
