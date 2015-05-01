1ddd3bd6c2991bfa303f808c3639af78cfb08bda
Patryk Mikos <usciskajkaktusa@gmail.com>
2015-05-01 18:14:08 +0200

    Comments: IHistogramManager fully commented.

--- src/core/IHistogramManager.h
+++ src/core/IHistogramManager.h
@@ -11,12 +11,31 @@ using namespace std;
 namespace AcerDet {
 	namespace core {
 		
+		/**
+		 * Abstract interface describing contracts for any external implementation of histogram manager
+		 * supported by AcerDET.
+		 */
 		class IHistogramManager {
 		public:
+		
+			/**
+			 * Virtual destructor.
+			 */
 			virtual ~IHistogramManager() {}
 			
+			/**
+			 * Initialize histogram manager.
+			 */
 			virtual void init() = 0;
 			
+			/**
+			 * Create new instance of histogram and store it's reference in histogram manager.
+			 * \param id new histogram id - should be unique in manager.
+			 * \param title new histogram name.
+			 * \param blocks new histogram resolution.
+			 * \param minVal minimal value which can be stored in histogram.
+			 * \param maxVal maximal value which can be stored in histogram.
+			 */
 			virtual void registerHistogram(
 				Int32_t id,
 				const string& title,
@@ -24,8 +43,17 @@ namespace AcerDet {
 				Real64_t minVal,
 				Real64_t maxVal ) = 0;
 				
+			/**
+			 * Add new weighted record to histogram.
+			 * \param id histogram id to store record in.
+			 * \param value value to store.
+			 * \param weight weight of stored record.
+			 */
 			virtual void insert( Int32_t id, Real64_t value, Real64_t weight = 1.0) = 0;
 			
+			/**
+			 * Dump all histogram data to file system.
+			 */
 			virtual void storeHistograms() = 0;
 		};
 
