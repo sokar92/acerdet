a81dc9bf683b1bdd98fca96778a34635d848f0fb
Patryk Mikos <usciskajkaktusa@gmail.com>
2015-05-01 17:18:43 +0200

    Comments: Smearing fully commented.

--- src/core/Smearing.h
+++ src/core/Smearing.h
@@ -9,28 +9,43 @@
 
 namespace AcerDet {
 	namespace core {
+		
+		/**
+		 * Utility class with methods for energy smearing computation.
+		 */
 		class Smearing {
 		private:
-			static std::default_random_engine generator; /*!< detailed description  */
-			static std::normal_distribution<Real64_t> distribution; /*!< detailed description  */
+			static std::default_random_engine generator;             /*!< C++ Standard library random variable generator. */
+			static std::normal_distribution<Real64_t> distribution;  /*!< Variable from normal distribution over doubles. */
 		public:
-			/*
-			 * parametrizes electron resolution 
+			
+			/**
+			 * Computes energy smearing for electrons.
+			 * \param ene electron energy.
+			 * \return electron energy after smearing.
 			 */
 			static Real64_t forElectron(Real64_t ene);
 			
-			/*
-			 * parametrizes smearing for calorymetric energy deposition
+			 /**
+			 * Computes energy smearing for hadrons.
+			 * \param ene hadron energy.
+			 * \param eta hadronic particle angle.
+			 * \param caloth calorimetric energy deposition factor.
+			 * \return hadron energy after smearing.
 			 */
 			static Real64_t forHadron(Real64_t ene, Real64_t eta, Real64_t caloth);
 			
-			/*
-			 * parametrizes muon resolution 
+			/**
+			 * Computes energy smearing for muons.
+			 * \param pt muon energy.
+			 * \return muon energy after smearing.
 			 */
 			static Real64_t forMuon(Real64_t pt);
 			
-			/*
-			 * parametrizes photon resolution 
+			/**
+			 * Computes energy smearing for photons.
+			 * \param ene photon energy.
+			 * \return photon energy after smearing.
 			 */
 			static Real64_t forPhoton(Real64_t ene);
 		};
