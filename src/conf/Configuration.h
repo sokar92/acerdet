4a081c23f00b11becd31335f1c96f73e32491ef1
Patryk Mikos <usciskajkaktusa@gmail.com>
2015-05-02 10:41:02 +0200

    Comments: Configuration commented

--- src/conf/Configuration.h
+++ src/conf/Configuration.h
@@ -9,7 +9,7 @@ using namespace std;
 namespace AcerDet {
 	namespace conf {
 
-		//! Global configuration
+		//! Global AcerDET configuration.
 		/*!
 		 * A set of parameters used by analyse algorithms.
 		 */
@@ -20,18 +20,31 @@ namespace AcerDet {
 			 * A set of commonly used parameters.
 			 */
 			struct _Flag {
-				int HistogramId;	/*!< detailed description  */
-				bool Smearing;		/*!< detailed description  */
-				bool BField;		/*!< detailed description  */
-				int SusyParticle;	/*!< detailed description  */
+				int HistogramId;       /*!< detailed description  */
+				bool Smearing;         /*!< Should AcerDET use energy smearing? */
+				bool BField;           /*!< detailed description  */
+				int SusyParticle;      /*!< PDGid of SUSY particle. */
 
-				bool BCJetsLabeling;	/*!< detailed description  */
-				bool TauJetsLabeling;	/*!< detailed description  */
-				bool JetCalibration;	/*!< detailed description  */
+				bool BCJetsLabeling;	/*!< Should AcerDET label b-jets and c-jets? */
+				bool TauJetsLabeling;	/*!< Should AcerDET label tau jets? */
+				bool JetCalibration;	/*!< Should AcerDET use calibration for jets? */
 
+				/**
+				 * Default constructor.
+				 * \return new instance of global configuration.
+				 */
 				_Flag();
 
-				void read( const string& );
+				/**
+				 * Reads common (globally used) configuration from string.
+				 * \param str string to read from.
+				 */
+				void read( const string& str );
+				
+				/**
+				 * Converts current global configuration into string.
+				 * \return converted configuration.
+				 */
 				string write( ) const;
 
 			} Flag;
@@ -41,16 +54,29 @@ namespace AcerDet {
 			 * A set of parameters used by analyse::Cell algorithm.
 			 */
 			struct _Cell {
-				double RapidityCoverage;	/*!< detailed description  */
-				double MinpT;			/*!< detailed description  */
-				double MinEt;			/*!< detailed description  */
-				double EtaTransition;	/*!< detailed description  */
-				double GranularityEta;	/*!< detailed description  */
-				double GranularityPhi;	/*!< detailed description  */
+				double RapidityCoverage;  /*!< detailed description  */
+				double MinpT;             /*!< Minimal particle energy to treat it as interesting object. */
+				double MinEt;             /*!< Minimal energy to mark parton as a Cell. */
+				double EtaTransition;     /*!< detailed description  */
+				double GranularityEta;    /*!< Cell eta coordinate granularity. */
+				double GranularityPhi;    /*!< Cell phi coordinate granularity. */
 
+				/**
+				 * Default constructor.
+				 * \return new instance of analyse::Cell configuration.
+				 */
 				_Cell();
 
-				void read( const string& );
+				/**
+				 * Reads analyse::Cell configuration from string.
+				 * \param str string to read from.
+				 */
+				void read( const string& str );
+				
+				/**
+				 * Converts current analyse::Cell configuration into string.
+				 * \return converted configuration.
+				 */
 				string write( ) const;
 
 			} Cell;
@@ -60,14 +86,27 @@ namespace AcerDet {
 			 * A set of parameters used by analyse::Cluster algorithm.
 			 */
 			struct _Cluster {
-				double RapidityCoverage;	/*!< detailed description  */
-				double ConeR;		/*!< detailed description  */
-				double MinEt;		/*!< detailed description  */
-				double MinEtInit;	/*!< detailed description  */
+				double RapidityCoverage;  /*!< detailed description  */
+				double ConeR;             /*!< detailed description  */
+				double MinEt;             /*!< detailed description  */
+				double MinEtInit;         /*!< detailed description  */
 
+				/**
+				 * Default constructor.
+				 * \return new instance of analyse::Cluster configuration.
+				 */
 				_Cluster();
 
-				void read( const string& );
+				/**
+				 * Reads analyse::Cluster configuration from string.
+				 * \param str string to read from.
+				 */
+				void read( const string& str );
+				
+				/**
+				 * Converts current analyse::Cluster configuration into string.
+				 * \return converted configuration.
+				 */
 				string write( ) const;
 
 			} Cluster;
@@ -77,15 +116,28 @@ namespace AcerDet {
 			 * A set of parameters used by analyse::Muon algorithm.
 			 */
 			struct _Muon {
-				double MinMomenta;	/*!< detailed description  */
-				double MaxEta;		/*!< detailed description  */
-				double MinIsolRlj;	/*!< detailed description  */
-				double ConeR;		/*!< detailed description  */
-				double MaxEnergy;	/*!< detailed description  */
+				double MinMomenta;  /*!< detailed description  */
+				double MaxEta;      /*!< detailed description  */
+				double MinIsolRlj;  /*!< detailed description  */
+				double ConeR;       /*!< detailed description  */
+				double MaxEnergy;   /*!< detailed description  */
 
+				/**
+				 * Default constructor.
+				 * \return new instance of analyse::Muon configuration.
+				 */
 				_Muon();
 
-				void read( const string& );
+				/**
+				 * Reads analyse::Muon configuration from string.
+				 * \param str string to read from.
+				 */
+				void read( const string& str );
+				
+				/**
+				 * Converts current analyse::Muon configuration into string.
+				 * \return converted configuration.
+				 */
 				string write( ) const;
 
 			} Muon;
@@ -95,16 +147,29 @@ namespace AcerDet {
 			 * A set of parameters used by analyse::Photon algorithm.
 			 */
 			struct _Photon {
-				double MinMomenta;	/*!< detailed description  */
-				double MaxEta;		/*!< detailed description  */
-				double MinJetsRlj;	/*!< detailed description  */
-				double MinIsolRlj;	/*!< detailed description  */
-				double ConeR;		/*!< detailed description  */
-				double MaxEnergy;	/*!< detailed description  */
+				double MinMomenta;  /*!< detailed description  */
+				double MaxEta;      /*!< detailed description  */
+				double MinJetsRlj;  /*!< detailed description  */
+				double MinIsolRlj;  /*!< detailed description  */
+				double ConeR;       /*!< detailed description  */
+				double MaxEnergy;   /*!< detailed description  */
 
+				/**
+				 * Default constructor.
+				 * \return new instance of analyse::Photon configuration.
+				 */
 				_Photon();
 
-				void read( const string& );
+				/**
+				 * Reads analyse::Electron configuration from string.
+				 * \param str string to read from.
+				 */
+				void read( const string& str );
+				
+				/**
+				 * Converts current analyse::Photon configuration into string.
+				 * \return converted configuration.
+				 */
 				string write( ) const;
 
 			} Photon;
@@ -114,16 +179,29 @@ namespace AcerDet {
 			 * A set of parameters used by analyse::Electron algorithm.
 			 */
 			struct _Electron {
-				double MinMomenta;	/*!< detailed description  */
-				double MaxEta;		/*!< detailed description  */
-				double MinJetsRlj;	/*!< detailed description  */
-				double MinIsolRlj;	/*!< detailed description  */
-				double ConeR;		/*!< detailed description  */
-				double MaxEnergy;	/*!< detailed description  */
+				double MinMomenta;  /*!< detailed description  */
+				double MaxEta;      /*!< detailed description  */
+				double MinJetsRlj;  /*!< detailed description  */
+				double MinIsolRlj;  /*!< detailed description  */
+				double ConeR;       /*!< detailed description  */
+				double MaxEnergy;   /*!< detailed description  */
 
+				/**
+				 * Default constructor.
+				 * \return new instance of analyse::Electron configuration.
+				 */
 				_Electron();
 
-				void read( const string& );
+				/**
+				 * Reads analyse::Electron configuration from string.
+				 * \param str string to read from.
+				 */
+				void read( const string& str );
+				
+				/**
+				 * Converts current analyse::Electron configuration into string.
+				 * \return converted configuration.
+				 */
 				string write( ) const;
 
 			} Electron;
@@ -133,12 +211,25 @@ namespace AcerDet {
 			 * A set of parameters used by analyse::Jet algorithm.
 			 */
 			struct _Jet {
-				double RapidityCoverage;	/*!< detailed description  */
-				double MinEnergy;	/*!< detailed description  */
+				double RapidityCoverage;  /*!< detailed description  */
+				double MinEnergy;         /*!< detailed description  */
 
+				/**
+				 * Default constructor.
+				 * \return new instance of analyse::Jet configuration.
+				 */
 				_Jet();
 
-				void read( const string& );
+				/**
+				 * Reads analyse::Jet configuration from string.
+				 * \param str string to read from.
+				 */
+				void read( const string& str );
+				
+				/**
+				 * Converts current analyse::Jet configuration into string.
+				 * \return converted configuration.
+				 */
 				string write( ) const;
 
 			} Jet;
@@ -148,13 +239,26 @@ namespace AcerDet {
 			 * A set of parameters used by analyse::BJet algorithm.
 			 */
 			struct _BJet {
-				double MinMomenta;	/*!< detailed description  */
-				double MaxEta;		/*!< detailed description  */
-				double MaxRbj;		/*!< detailed description  */
+				double MinMomenta;  /*!< detailed description  */
+				double MaxEta;      /*!< detailed description  */
+				double MaxRbj;      /*!< detailed description  */
 
+				/**
+				 * Default constructor.
+				 * \return new instance of analyse::BJet configuration.
+				 */
 				_BJet();
 
-				void read( const string& );
+				/**
+				 * Reads analyse::BJet configuration from string.
+				 * \param str string to read from.
+				 */
+				void read( const string& str );
+				
+				/**
+				 * Converts current analyse::BJet configuration into string.
+				 * \return converted configuration.
+				 */
 				string write( ) const;
 
 			} BJet;
@@ -164,13 +268,26 @@ namespace AcerDet {
 			 * A set of parameters used by analyse::CJet algorithm.
 			 */
 			struct _CJet {
-				double MinMomenta;	/*!< detailed description  */
-				double MaxEta;		/*!< detailed description  */
-				double MaxRcj;		/*!< detailed description  */
+				double MinMomenta;  /*!< detailed description  */
+				double MaxEta;      /*!< detailed description  */
+				double MaxRcj;      /*!< detailed description  */
 
+				/**
+				 * Default constructor.
+				 * \return new instance of analyse::CJet configuration.
+				 */
 				_CJet();
 
-				void read( const string& );
+				/**
+				 * Reads analyse::CJet configuration from string.
+				 * \param str string to read from.
+				 */
+				void read( const string& str );
+				
+				/**
+				 * Converts current analyse::CJet configuration into string.
+				 * \return converted configuration.
+				 */
 				string write( ) const;
 
 			} CJet;
@@ -180,14 +297,27 @@ namespace AcerDet {
 			 * A set of parameters used by analyse::Tau algorithm.
 			 */
 			struct _Tau {
-				double MinpT;	/*!< detailed description  */
-				double MaxEta;	/*!< detailed description  */
-				double MinR;	/*!< detailed description  */
-				double MaxR;	/*!< detailed description  */
+				double MinpT;   /*!< detailed description  */
+				double MaxEta;  /*!< detailed description  */
+				double MinR;    /*!< detailed description  */
+				double MaxR;    /*!< detailed description  */
 
+				/**
+				 * Default constructor.
+				 * \return new instance of analyse::Tau configuration.
+				 */
 				_Tau();
 
-				void read( const string& );
+				/**
+				 * Reads analyse::Tau configuration from string.
+				 * \param str string to read from.
+				 */
+				void read( const string& str );
+				
+				/**
+				 * Converts current analyse::Tau configuration into string.
+				 * \return converted configuration.
+				 */
 				string write( ) const;
 
 			} Tau;
@@ -197,32 +327,48 @@ namespace AcerDet {
 			 * A set of parameters used by analyse::Mis algorithm.
 			 */
 			struct _Misc {
-				double MinEt;	/*!< detailed description  */
+				double MinEt;  /*!< detailed description  */
 
+				/**
+				 * Default constructor.
+				 * \return new instance of missing energy configuration.
+				 */
 				_Misc();
 
-				void read( const string& );
+				/**
+				 * Reads Missing energy configuration from string.
+				 * \param str string to read from.
+				 */
+				void read( const string& str );
+				
+				/**
+				 * Converts current Missing energy configuration into string.
+				 * \return converted configuration.
+				 */
 				string write( ) const;
 
 			} Misc;
 
-			//! Save configuration to specific file
-			/*!
-			 * \param configuration configuration to save
-			 * \param fileName name of destination file
+			/** Save configuration to file.
+			 * \param configuration configuration to save.
+			 * \param fileName name of destination file.
 			 */
 			static void save(
 				const Configuration& configuration,
 				const std::string& fileName );
 
-			//! Read configuration from given file
-			/*!
-			 * \param fileName name of file to read from
+			/**
+			 * Read configuration from given file.
+			 * \param fileName name of file to read from.
+			 * \return Configuration read from given file.
 			 */
 			static Configuration fromFile(
 				const std::string& fileName );
 
-			//! Returns default configuration
+			/**
+			 * Creates a default Configuration instance. 
+			 * \return default configuration
+			 */
 			static Configuration getDefault();
 		};
 
