d03530f6651bff78a163f3486727ca8febcb1653
elarichterwas <Elzbieta.Richter-Was@uj.edu.pl>
2015-06-10 19:05:40 +0200

    cleaning Electron + modifying isHardProcess

--- src/analyse/Electron.cpp
+++ src/analyse/Electron.cpp
@@ -207,6 +207,8 @@ void Electron::analyseRecord( const io::InputRecord& irecord, io::OutputRecord& 
 		
 		if (part.type == PT_ELECTRON) {  
 
+                printf("accepted: pdgId=%d, barcode=%d, mother=%d\n",parts[i].pdg_id, parts[i].barcode, parts[i].mother); 
+
 			Real64_t ENER = 0.0;
 			Bool_t ISOL = true;
 
--- src/core/Functions.cpp
+++ src/core/Functions.cpp
@@ -2,9 +2,11 @@
 using namespace AcerDet::core;
 
 Bool_t AcerDet::core::isHardProcess(const vector<Particle>& parts, int i) {
-	// particle is hard if is final
-	if (parts[i].status != PS_FINAL)
-		return false;
+  //    particle is hard if is final
+  //    one can use it if the below check is done recursively, i.e
+  //    requiring any accessor to be H, W, or Z boson
+  //	if (parts[i].status != PS_FINAL)
+  //		return false;
 
 	// if mother exists should be H, W, or Z boson {23, 24, 25}
 	if (parts[i].hasMother()) {
