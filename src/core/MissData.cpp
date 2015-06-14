23edd93521599872834f18b97b32776775708cc1
elarichterwas <Elzbieta.Richter-Was@uj.edu.pl>
2015-06-14 19:40:28 +0200

    completing initialisation of MissData

--- src/core/MissData.cpp
+++ src/core/MissData.cpp
@@ -5,10 +5,12 @@ MissData::MissData() :
 	PXREC (0.0),
 	PYREC (0.0),
 	PXSUM (0.0),
-	PYSUM(0.0),
+	PYSUM (0.0),
 	PXXCALO (0.0),
 	PYYCALO (0.0),
-	SUMET(0.0)
+	PXNUE (0.0),
+	PYNUE (0.0),
+	SUMET (0.0)
 {}
 
 void MissData::clear() {
@@ -18,5 +20,7 @@ void MissData::clear() {
 	PYSUM = 0.0;
 	PXXCALO = 0.0;
 	PYYCALO = 0.0;
+	PXNUE = 0.0;
+	PYNUE = 0.0;
 	SUMET = 0.0;
 }
